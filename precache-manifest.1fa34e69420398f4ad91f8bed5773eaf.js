2f3267a5c578aac4905a8578dbadbfccfa101dff
Martin Kruger <martin@martinkruger.com>
2020-01-12 19:24:52 +0200

    Updates

---
+++ precache-manifest.1fa34e69420398f4ad91f8bed5773eaf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "600780ae7b6ec7f079345908259bec4c",
+    "url": "/weather-forecast/index.html"
+  },
+  {
+    "revision": "4c7fb05f6189dbbdba23",
+    "url": "/weather-forecast/static/css/main.94b3af54.chunk.css"
+  },
+  {
+    "revision": "ba4b96e1982a1dbb6aca",
+    "url": "/weather-forecast/static/js/2.6b5e137f.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/weather-forecast/static/js/2.6b5e137f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c7fb05f6189dbbdba23",
+    "url": "/weather-forecast/static/js/main.dc726687.chunk.js"
+  },
+  {
+    "revision": "a99f85a62baa30bfd9ca",
+    "url": "/weather-forecast/static/js/runtime-main.e7f16e4a.js"
+  },
+  {
+    "revision": "51eb47bd13636c3369cb61bf9ba35bf7",
+    "url": "/weather-forecast/static/media/background1.51eb47bd.jpeg"
+  },
+  {
+    "revision": "1be7acfe2e46c01f5b1e1174779c25fc",
+    "url": "/weather-forecast/static/media/drop-no-percentage.1be7acfe.svg"
+  },
+  {
+    "revision": "a17d993450284356e0ea7e1406573d48",
+    "url": "/weather-forecast/static/media/gauge-ungraded.a17d9934.svg"
+  },
+  {
+    "revision": "89aaef2a99515c7a9b9f75a4909bb8d2",
+    "url": "/weather-forecast/static/media/sunrise-no-arrow.89aaef2a.svg"
+  },
+  {
+    "revision": "21268c1615a4cafff9f9e4541dce25ca",
+    "url": "/weather-forecast/static/media/wind-simpler.21268c16.svg"
+  }
+]);
